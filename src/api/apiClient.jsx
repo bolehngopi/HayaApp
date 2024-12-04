379a54df0b44b504e8b9b41c766545890f291b86
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-12-04 13:16:30 +0700

    fix: set default baseURL for apiClient to localhost if VITE_APP_API is not defined

--- src/api/apiClient.jsx
+++ src/api/apiClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_APP_API, // Change to your Laravel API URL
+  baseURL: import.meta.env.VITE_APP_API || "http://localhost:3000", // Change to your Laravel API URL
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
